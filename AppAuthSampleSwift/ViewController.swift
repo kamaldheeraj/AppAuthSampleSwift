b3d1cfc6d512a5cc960a7794ce869e0b5400dc52
Kamal Dandamudi <kamal.dandamudi@vensi.com>
2016-05-25 10:42:52 -0500

    Initial Commit

---
+++ AppAuthSampleSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppAuthSampleSwift
+//
+//  Created by Kamal Dandamudi on 5/25/16.
+//  Copyright © 2016 SillyApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
